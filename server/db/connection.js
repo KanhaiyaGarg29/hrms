bc7eeaeb8b3f90b7900a1d113758afdf005eab33
kashishsingla324 <kashishsingla324@gmail.com>
2024-05-12 22:33:29 +0530

    done

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// const uri = "mongodb+srv://kashishsingla324:Kash0211@cluster0.ncyyweg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri="mongodb://127.0.0.1:27017/hrDB"
+const uri = "mongodb+srv://kashishsingla324:Kash0211@cluster0.ncyyweg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri="mongodb://127.0.0.1:27017/hrDB"
 
 const connectToDb = () =>{
     mongoose.connect(uri,{
